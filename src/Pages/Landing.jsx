b6c27ce72a99e3b640900f6621c5456fb1971797
ardpm <ardiansyahpm224@gmail.com>
2023-02-23 15:06:50 +0700

    setup fronted

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Box,
+    Flex,
+    Container,
+    Text
+} from '@chakra-ui/react';
+
+function LandingPage() {
+    return (
+            <Box margin={'auto'}>
+                Ardi
+            </Box>
+
+
+    );
+}
+
+export default LandingPage;
